988ca3e7dff84176b4d9438f83a9b0f2901af6f1
Lukáš Veteška <glararan@glararan.eu>
2018-10-08 17:44:21 +0200

    Application: simplify validation layers

---
+++ VulkanTerrainEditor/Glm.h
@@ -0,0 +1,4 @@
+#ifndef GLM_H
+#define GLM_H
+
+#endif // GLM_H
---
+++ VulkanTerrainEditor/shaders/terrain_vert.glsl
@@ -0,0 +1,24 @@
+#version 450
+
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec3 UV;
+
+layout (location = 0) out vec3 outNormal;
+layout (location = 1) out vec2 outUV;
+
+out gl_PerVertex
+{
+    vec4 gl_Position;
+};
+
+void main()
+{
+    gl_Position = vec4(pos, 1.0);
+
+    outNormal = normal;
+    outUV = UV;
+}
