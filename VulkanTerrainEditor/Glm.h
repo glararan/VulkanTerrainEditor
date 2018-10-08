4bc85aa0dc38a4a9203efe6d6a85fceda9b58017
Lukáš Veteška <glararan@glararan.eu>
2018-10-08 17:44:55 +0200

    Glm.h

--- VulkanTerrainEditor/Glm.h
+++ VulkanTerrainEditor/Glm.h
@@ -1,4 +1,13 @@
 #ifndef GLM_H
 #define GLM_H
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <gli/gli.hpp>
+
 #endif // GLM_H
